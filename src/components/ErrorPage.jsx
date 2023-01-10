17e1fd4ac8d3e6a9bc2cfb7ee4bce2148fdba4d3
Marcos Mendoza <81839444+MarcosFMC@users.noreply.github.com>
2023-01-10 16:37:17 -0300

    x

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -8,7 +8,7 @@ const ErrorPage = () => {
     <SCErrorPage>
       <h2>Error 404: Not found</h2>
       <Snackbar message="Ruta no encontrada" danger={true}>
-        <Link className="router-link" to="/Firebase-form">
+        <Link className="router-link" to="/Firebase-Form/">
           Click para ir a una ruta valida
         </Link>
       </Snackbar>
